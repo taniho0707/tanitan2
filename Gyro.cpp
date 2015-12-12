ac1a0f41ca24af555ddbe7a2cbb3c90d520c52ae
taniho_0707 <taniho_0707@yahoo.co.jp>
2015-12-12 13:15:16 +0900

    Add Gyro which can read only 'WHO_AM_I'

--- Gyro.cpp
+++ Gyro.cpp
@@ -9,17 +9,19 @@ void Gyro::init(){
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI3, ENABLE);
+
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_StructInit(&GPIO_InitStructure);
-	// GPIO_InitStructure.GPIO_Pin = SPI3_SCK_Pin | SPI1_MISO_Pin | SPI1_MOSI_Pin;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	GPIO_SetBits(GPIOA, GPIO_Pin_15);
 
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
@@ -30,27 +32,25 @@ void Gyro::init(){
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource15, GPIO_AF_SPI3);
+//	GPIO_PinAFConfig(GPIOA, GPIO_PinSource15, GPIO_AF_SPI3);
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_SPI3);
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_SPI3);
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource12, GPIO_AF_SPI3);
 
 	SPI_InitTypeDef SPI_InitStructure;
+
 	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
 	SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
 	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
-	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
-	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
-	SPI_InitStructure.SPI_NSS = SPI_NSS_Hard | SPI_NSSInternalSoft_Set;
+	SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
+	SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
+//	SPI_InitStructure.SPI_NSS = SPI_NSS_Hard | SPI_NSSInternalSoft_Set;
+	SPI_InitStructure.SPI_NSS = SPI_NSSInternalSoft_Set | SPI_NSS_Soft;
 	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
 	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+	SPI_InitStructure.SPI_CRCPolynomial = 7;
 	SPI_Init(SPI3, &SPI_InitStructure);
 	SPI_Cmd(SPI3, ENABLE);
-
-	SPI_I2S_SendData(SPI3, 0x0F);
-	SPI_I2S_GetFlagStatus(SPI3, SPI_I2S_FLAG_TXE);
-	uint16_t ret = SPI_I2S_ReceiveData(SPI3);
-	if(ret == 0x69);
 }
 
 
--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(void){
 
 	Led::on(LedNumbers::FRONT);
 	Led::on(LedNumbers::RIGHT);
-	Led::on(LedNumbers::LEFT1);
+//	Led::on(LedNumbers::LEFT1);
 	Led::on(LedNumbers::LEFT2);
 	Led::on(LedNumbers::LEFT3);
 	Timer::wait_ms(500);
@@ -26,6 +26,24 @@ int main(void){
 	Led::off(LedNumbers::LEFT2);
 	Led::off(LedNumbers::LEFT3);
 
+	Gyro::init();
+
+	Timer::wait_ms(500);
+
+	uint16_t ret = 0xFF;
+	GPIO_ResetBits(GPIOA, GPIO_Pin_15);
+	while (SPI_I2S_GetFlagStatus(SPI3, SPI_I2S_FLAG_TXE) == RESET);
+	SPI_I2S_SendData(SPI3, 0x8F);
+	while (SPI_I2S_GetFlagStatus(SPI3, SPI_I2S_FLAG_TXE) == RESET);
+	SPI_I2S_SendData(SPI3, 0x00);
+	while (SPI_I2S_GetFlagStatus(SPI3, SPI_I2S_FLAG_RXNE) == RESET);
+	ret = SPI_I2S_ReceiveData(SPI3);
+	while (SPI_I2S_GetFlagStatus(SPI3, SPI_I2S_FLAG_RXNE) == RESET);
+	ret = SPI_I2S_ReceiveData(SPI3);
+	while (SPI_I2S_GetFlagStatus(SPI3, SPI_I2S_FLAG_TXE) == RESET);
+	GPIO_SetBits(GPIOA, GPIO_Pin_15);
+	if(ret == 0x69) Led::on(LedNumbers::RIGHT);
+	else Led::off(LedNumbers::FRONT);
 
 	while(true){
 		// if(Switch::isPushing(SwitchNumbers::LEFT))
