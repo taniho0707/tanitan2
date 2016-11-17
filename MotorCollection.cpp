73d790f37b656044a8e2f708b2bc4581a1633c1a
taniho_0707 <taniho_0707@yahoo.co.jp>
2016-11-17 21:34:58 +0900

    前壁補正を素早くした

--- MotorCollection.cpp
+++ MotorCollection.cpp
@@ -5,7 +5,7 @@
 
 MotorCollection::MotorCollection() :
 	TIMEOUT(10000),
-	GAIN_LIN(0.0004f),
+	GAIN_LIN(0.0005f),
 	GAIN_RAD(0.6f)
 {
 	
@@ -17,12 +17,14 @@ bool MotorCollection::collectionByFrontDuringStop(){
 	int32_t dif_right = static_cast<int32_t>(wall->getDiffValue(SensorPosition::FRight));
 	float dif_lin = (dif_right + dif_left) / 2;
 	float dif_rad = dif_right - dif_left;
+	float tmp;
 	while(Timer::getTime() < start_time + TIMEOUT){
 		dif_left = wall->getDiffValue(SensorPosition::FLeft);
 		dif_right = wall->getDiffValue(SensorPosition::FRight);
 		dif_lin = (dif_right + dif_left) / 2;
 		dif_rad = dif_right - dif_left;
-		mc->setVelocity(-1.0f * GAIN_LIN * dif_lin);
+		tmp = min(abs(dif_lin*GAIN_LIN), 0.3f);
+		mc->setVelocity(((dif_lin < 0)-(dif_lin > 0)) * tmp);
 		mc->setRadVelocity(0.0f);
 		if(abs(GAIN_LIN * dif_lin) < 0.02f) break;
 		Timer::wait_ms(1);
@@ -32,7 +34,8 @@ bool MotorCollection::collectionByFrontDuringStop(){
 		dif_right = wall->getDiffValue(SensorPosition::FRight);
 		dif_lin = (dif_right + dif_left) / 2;
 		dif_rad = dif_right - dif_left;
-		mc->setVelocity(-1.0f * GAIN_LIN * dif_lin);
+		tmp = min(abs(dif_lin*GAIN_LIN), 0.3f);
+		mc->setVelocity(((dif_lin < 0)-(dif_lin > 0)) * tmp);
 		mc->setRadVelocity(GAIN_RAD * dif_rad);
 		if(abs(GAIN_LIN * dif_lin) < 0.02f && abs(GAIN_RAD * dif_rad) < 0.5f) break;
 		Timer::wait_ms(1);
